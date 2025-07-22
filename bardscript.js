735925b6c7509b340932c4951d20c70ecf108199
Abdullah Al Shishir <abdullahshishir2018@gmail.com>
2025-07-22 16:47:30 +0600

    Update bardscript.js

--- bardscript.js
+++ bardscript.js
@@ -1,10 +1,8 @@
-// Target Element
 
 const cookieBanner=document.getElementById('cookieBanner');
 const acceptButton=document.getElementById('acceptCookies');
 const closeButton=document.getElementById('closeBanner');
 
-// Setting Parameter
 const showBanner=()=>{
     cookieBanner.classList.add('show')
 }
@@ -12,10 +10,8 @@ const showBanner=()=>{
 const hideBanner=()=>{
     cookieBanner.classList.remove('show')
 }
-// Done Setting Parameter 
+ 
 
-
-// Adding Event
 acceptButton.addEventListener('click',()=>{
 hideBanner();
 });
@@ -23,7 +19,6 @@ hideBanner();
 closeButton.addEventListener('click',()=>{
 hideBanner();
 });
-//Finished Adding Event
 
 window.onload=()=>{
     showBanner();
